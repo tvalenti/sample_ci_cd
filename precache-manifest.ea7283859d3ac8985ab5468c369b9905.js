b458ff64a557c5712aeaeac02da27b961f71db9b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-01 23:57:30 +0000

    Deploy tvalenti/sample_ci_cd to github.com/tvalenti/sample_ci_cd.git:gh-pages

---
+++ precache-manifest.ea7283859d3ac8985ab5468c369b9905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "37d3000a2696df65d69a",
+    "url": "/sample_ci_cd/static/css/main.bdc74e2d.chunk.css"
+  },
+  {
+    "revision": "37d3000a2696df65d69a",
+    "url": "/sample_ci_cd/static/js/main.bb363a8d.chunk.js"
+  },
+  {
+    "revision": "099730fc2f69f7a7955b",
+    "url": "/sample_ci_cd/static/js/runtime~main.70a8bfc3.js"
+  },
+  {
+    "revision": "8a8ecc960f2c4bb182ac",
+    "url": "/sample_ci_cd/static/js/2.fc8106d7.chunk.js"
+  },
+  {
+    "revision": "0c0db3a6fb8aa13050f2dbb843bc46b7",
+    "url": "/sample_ci_cd/index.html"
+  }
+];
